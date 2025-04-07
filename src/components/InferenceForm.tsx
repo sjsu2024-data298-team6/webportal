38b93610ea8f9d89cee693c9194b82a77dffc66e
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-04-07 15:33:35 -0700

    add loading indicator for model loading on webcam page

--- src/components/InferenceForm.tsx
+++ src/components/InferenceForm.tsx
@@ -11,6 +11,7 @@ const videoConstraints = {
 
 export default function InferenceForm() {
   const [isCaptureEnable, setCaptureEnable] = useState(false);
+  const [isLoadingModel, setIsLoadingModel] = useState(true);
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef(null);
   const [model, setModel] = useState<any>(null); // State to store the model
@@ -19,6 +20,7 @@ export default function InferenceForm() {
       const yolov8 = await tf.loadGraphModel(
         `${window.location.href}/yolov8n_web_model/model.json`,
       ); // load model
+      setIsLoadingModel(false);
 
       // warming up model
       const dummyInput = tf.ones(yolov8.inputs[0].shape!);
@@ -35,16 +37,25 @@ export default function InferenceForm() {
 
   return (
     <>
-      <header>
-        <h1>Webcam</h1>
-      </header>
       {isCaptureEnable || (
-        <button onClick={() => setCaptureEnable(true)}>Open Webcam</button>
+        <button
+          className={
+            isLoadingModel
+              ? "rounded-xl border border-black bg-gray-300 px-4 py-1"
+              : "rounded-xl border border-black bg-blue-400 px-4 py-1 hover:bg-blue-500"
+          }
+          onClick={() => setCaptureEnable(true)}
+        >
+          {isLoadingModel ? "Loading..." : "Open Webcam"}
+        </button>
       )}
       {isCaptureEnable && (
         <>
           <div>
-            <button onClick={() => setCaptureEnable(false)}>
+            <button
+              className="rounded-xl border border-black bg-red-500 px-4 py-1 hover:bg-red-600"
+              onClick={() => setCaptureEnable(false)}
+            >
               Close Webcam
             </button>
           </div>
