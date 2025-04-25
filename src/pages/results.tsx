a6e27ce5635d61cbac3169da094615496381bd21
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-04-24 17:46:57 -0700

    add blank results page

---
+++ src/pages/results.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function DatasetPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <Card>
+        <CardHeader>
+          <CardTitle>View results</CardTitle>
+        </CardHeader>
+        <CardContent>placeholder</CardContent>
+      </Card>
+    </div>
+  );
+}
