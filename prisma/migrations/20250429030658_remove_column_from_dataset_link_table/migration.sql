797238005e67377c86497c5cf4127cd0aa86481b
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-04-28 20:07:10 -0700

    update schema

---
+++ prisma/migrations/20250429030658_remove_column_from_dataset_link_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `extras` on the `DatasetLinkType` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "DatasetLinkType" DROP COLUMN "extras";
