29d623c222a4ec5787199546cd95492148ad71d9
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-05-04 01:34:16 -0700

    add detailed metrics to modal

--- src/components/ModelResultDialog.tsx
+++ src/components/ModelResultDialog.tsx
@@ -1,5 +1,13 @@
 import { Button } from "@/components/ui/button";
 import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
   Dialog,
   DialogContent,
   DialogDescription,
@@ -15,9 +23,34 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Badge } from "@/components/ui/badge";
 
+interface ClassMetrics {
+  name: string;
+  precision: number;
+  recall: number;
+  map50: number;
+  map5095: number;
+  iou: number;
+}
+
+interface AllMetrics {
+  precision: number;
+  recall: number;
+  map50: number;
+  map5095: number;
+  iou: number;
+  inference_time: number;
+  class_metrics: ClassMetrics[];
+}
+
+interface extraInfo {
+  YAML_URL: string;
+  wandb_logs: string;
+  detailed_metrics: AllMetrics | undefined;
+}
+
 interface modelResultType {
   datasetId: number;
-  extras: object;
+  extras: extraInfo;
   id: number;
   inferenceTime: number;
   iouScore: number;
@@ -93,31 +126,100 @@ const ModelResultDialog: React.FC<modelResultDialogProps> = ({ data }) => {
           </DialogDescription>
         </DialogHeader>
         <div className="my-2">
-          <span className="w-full font-bold">Metrics</span>
-          <div className="flex w-full flex-row">
-            <div className="w-1/2 justify-start">Inference Time (ms)</div>
-            <div className="w-1/2 justify-end text-right">
-              {data.inferenceTime.toFixed(3)}
-            </div>
-          </div>
-          <div className="flex w-full flex-row">
-            <div className="w-1/2 justify-start">IoU Score</div>
-            <div className="w-1/2 justify-end text-right">
-              {data.iouScore.toFixed(3)}
-            </div>
-          </div>
-          <div className="flex w-full flex-row">
-            <div className="w-1/2 justify-start">mAP 50 Score</div>
-            <div className="w-1/2 justify-end text-right">
-              {data.map50Score.toFixed(3)}
-            </div>
-          </div>
-          <div className="flex w-full flex-row">
-            <div className="w-1/2 justify-start">mAP 50-95 Score</div>
-            <div className="w-1/2 justify-end text-right">
-              {data.map5095Score.toFixed(3)}
-            </div>
-          </div>
+          {data.extras.detailed_metrics ? (
+            <>
+              <span className="w-full font-bold">Overall Metrics</span>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">Inference Time (ms)</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.extras.detailed_metrics.inference_time.toFixed(3)}
+                </div>
+              </div>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">IoU Score</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.extras.detailed_metrics.iou.toFixed(3)}
+                </div>
+              </div>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">mAP 50 Score</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.extras.detailed_metrics.map50.toFixed(3)}
+                </div>
+              </div>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">mAP 50-95 Score</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.extras.detailed_metrics.map5095.toFixed(3)}
+                </div>
+              </div>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">Precision</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.extras.detailed_metrics.precision.toFixed(3)}
+                </div>
+              </div>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">Recall</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.extras.detailed_metrics.recall.toFixed(3)}
+                </div>
+              </div>
+              <span className="mt-2 w-full font-bold">Per Class Metrics</span>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Name</TableHead>
+                    <TableHead>IoU</TableHead>
+                    <TableHead>mAP 50</TableHead>
+                    <TableHead>mAP 50-95</TableHead>
+                    <TableHead>Precision</TableHead>
+                    <TableHead>Recall</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {data.extras.detailed_metrics.class_metrics.map((row) => (
+                    <TableRow key={row.name}>
+                      <TableCell>{row.name}</TableCell>
+                      <TableCell>{row.iou.toFixed(3)}</TableCell>
+                      <TableCell>{row.map50.toFixed(3)}</TableCell>
+                      <TableCell>{row.map5095.toFixed(3)}</TableCell>
+                      <TableCell>{row.precision.toFixed(3)}</TableCell>
+                      <TableCell>{row.recall.toFixed(3)}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </>
+          ) : (
+            <>
+              <span className="w-full font-bold">Metrics</span>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">Inference Time (ms)</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.inferenceTime.toFixed(3)}
+                </div>
+              </div>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">IoU Score</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.iouScore.toFixed(3)}
+                </div>
+              </div>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">mAP 50 Score</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.map50Score.toFixed(3)}
+                </div>
+              </div>
+              <div className="flex w-full flex-row">
+                <div className="w-1/2 justify-start">mAP 50-95 Score</div>
+                <div className="w-1/2 justify-end text-right">
+                  {data.map5095Score.toFixed(3)}
+                </div>
+              </div>
+            </>
+          )}
         </div>
         <div className="my-2">
           <span className="w-full font-bold">Model Links</span>
@@ -144,14 +246,18 @@ const ModelResultDialog: React.FC<modelResultDialogProps> = ({ data }) => {
         </div>
         <div className="my-2">
           <span className="w-full font-bold">Extra information</span>
-          {Object.entries(data.extras).map(([key, value]) => (
-            <div className="flex w-full flex-row" key={key}>
-              <div className="w-1/2 justify-start">{key}</div>
-              <div className="w-1/2 justify-end text-right">
-                {renderExtraInfoValue(value)}
-              </div>
+          <div className="flex w-full flex-row">
+            <div className="w-1/2 justify-start">Model config YAML</div>
+            <div className="w-1/2 justify-end text-right">
+              {renderExtraInfoValue(data.extras.YAML_URL)}
             </div>
-          ))}
+          </div>
+          <div className="flex w-full flex-row">
+            <div className="w-1/2 justify-start">Weights & Biases Link</div>
+            <div className="w-1/2 justify-end text-right">
+              {renderExtraInfoValue(data.extras.wandb_logs)}
+            </div>
+          </div>
         </div>
         <DialogFooter>
           <Button variant={"destructive"}>Close</Button>
